a405a9b6bf516748b73322cb9afd7a2b51e9c280
Kawarai-S <kayawakakururu@icloud.com>
2023-04-19 15:08:27 +0900

    ハンバーガーメニュー実装

--- neko/js/main.js
+++ neko/js/main.js
@@ -1,11 +1,31 @@
 // jsを記述する際はここに記載していく
 $(function(){
   $('.slider').slick({
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      centerMode: true,
-      centerPadding: '30%'
+    autoplay:true,
+    // dots: true,
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
   });
 });
+
+$(function() {
+  var $body   = $('body');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // メニューの開閉
+  $btn.on( 'click', function() {
+    if ( ! $body.hasClass( open ) ) {
+      $body.addClass( open );
+    } else {
+      $body.removeClass( open );
+    }
+  });
+  // 背景の除去
+  $mask.on('click', function() {
+    $body.removeClass( open );
+  });
+} )(jQuery);
